faaec631eea55c67e3b8f82b9e5ebffdd58be163
styanr <chaus.oleh@gmail.com>
2024-01-18 16:48:22 +0200

    [fix] Fill in IClientService interface

--- VetClinicManager/DataServices/IClientService.cs
+++ VetClinicManager/DataServices/IClientService.cs
@@ -1,5 +1,11 @@
-﻿namespace VetClinicManager.DataServices;
+﻿using VetClinicManager.Model;
+
+namespace VetClinicManager.DataServices;
 
 internal interface IClientService
 {
+    Task<List<Client>> GetClientsAsync();
+    Task<Client> CreateClientAsync(Client client);
+    Task UpdateClientAsync(Client updatedClient);
+    Task DeleteClientAsync(int clientId);
 }
